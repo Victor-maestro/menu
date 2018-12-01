55b2e79df4b90702cad9033a487ef4316b29e5ca
Victor-maestro <victor_maestro@mail.ru>
2018-12-01 21:50:59 +0300

    Начало проекта. Установка переменных и базовые настройки.

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+var syntax        = 'sass'; // Синтаксис: sass или scss;
+
+var gulp          = require('gulp'),
+        browsersync   = require('browser-sync'), 
+        autoprefixer  = require('gulp-autoprefixer'),
+        cache         = require('gulp-cache'),
+        cleancss      = require('gulp-clean-css'),
+        concat        = require('gulp-concat'),
+        imagemin      = require('gulp-imagemin'),
+		gutil         = require('gulp-util' ),
+        notify        = require("gulp-notify"),
+        rename        = require('gulp-rename'),
+        sass          = require('gulp-sass'),
+		uglify        = require('gulp-uglify'),
+        del           = require('del');
+		
+
+// Scripts concat & minify
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+        
+		'app/js/common.js', // Всегда в конце
+		])
+	// .pipe(concat('scripts.min.js')) // Конкатенация js (опц.)
+	// .pipe(uglify()) // Минимизация js (опц.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('browser-sync', function() {
+	browsersync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+        // open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('sass', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+    // .pipe(rename({ suffix: '.min', prefix : '' })) // Mifify css (opt.)
+	.pipe(autoprefixer(['last 15 versions']))
+    .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} )) // --???
+});
+
+gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['sass']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browsersync.reload)
+});
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/**/*')
+	.pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ]))
+	.pipe(gulp.dest('dist/img')); 
+});
+
+gulp.task('build', ['removedist', 'imagemin', 'sass', 'js'], function() {
+
+	var buildFiles = gulp.src([
+		'app/*.html',
+        'app/*.php',
+		'app/.htaccess',
+		]).pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'app/css/main.css',
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src([
+		'app/js/*.js',
+		'app/js/common.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src([
+		'app/fonts/**/*',
+		]).pipe(gulp.dest('dist/fonts'));
+
+});
+
+
+gulp.task('removedist', function() { return del.sync('dist'); });
+gulp.task('clearcache', function () { return cache.clearAll(); });
+
+gulp.task('default', ['watch']);
---
+++ readme.md
@@ -0,0 +1,17 @@
+<h1><strong>Start_Template:</strong> <br>Стартовый шаблон на основе сборщика Gulp</h1>
+
+
+<p>Автор: <a href="http://m-web.site/" target="_blank">KRYZHNYI VIKTOR</a></p>
+
+
+<p>В комплекте шаблона "Start_Template" включено: <br>Gulp, Sass(Scss), Browsersync, Autoprefixer, Clean-CSS, Uglify, Imagemin, Rsync and Bower. Шаблон содержит <br><strong>.htaccess</strong> файл с настройками кэширования для веб-сервера.</p>
+
+
+<h2>Установка шаблона Start_Template</h2>
+
+<ol>
+	<li><a href="https://github.com/Victor-maestro/Start_Template/archive/master.zip">Загрузить</a> <strong>Start_Template_B4</strong> из GitHub;</li>
+	<li><strong>В командной строке</strong>;</li>
+	<li>Установка Node модулей: <strong>npm i</strong>;</li>
+	<li>Запуск шаблона: <strong>gulp</strong>.</li>
+</ol>
